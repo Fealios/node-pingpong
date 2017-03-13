bd287fba89ec918fd1c6feb2326930edf14c8ccd
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-03-13 13:40:40 -0700

    commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+
+gulp.task('myTask', function(){
+  console.log('hello gulp');
+});
+
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/pingpong-interface.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
